0f3b099e4b03cc369e10218a67299ab2bf3f3f54
Julio Orozco <julio.orozco@koombea.com>
2015-09-18 16:32:19 -0500

    first class

--- scripts/components/app.js
+++ scripts/components/app.js
@@ -1,15 +1,42 @@
 var React = require('react');
 var lista = require('../lista.json').people;
-
+console.log(lista);
 var {ul, li, h1, div, b} = React.DOM;
+var sortBy = require('sort-by');
 
-var app = React.createClass({
+var Trabajador = React.createClass({
 	render() {
 		return(
-     div({},
-      h1({}, "El equipo de Koombea"))
+      <li>
+      	{this.props.data.firstName} {this.props.data.lastName} email: 
+      	<a href={"mailto:" + this.props.data.email}>{this.props.data.email}</a>
+      </li>
 		);
 	}
 });
 
-module.exports = app;
+// var ListaTrabajadores = React.createClass({
+// 	render(){
+// 		var listaTrabajadores = lista.map((trabajador, i) => { return <Trabajador key={i} data={trabajador} />});
+// 		return(
+// 			<ul>
+// 				{listaTrabajadores}
+// 			</ul>
+// 		);
+// 	}
+// });
+
+var ListaTrabajadores = React.createClass({
+	render(){
+		var listaTrabajadores = lista.filter(function(el) { return el.role == "Web Development"; });
+		listaTrabajadores = listaTrabajadores.sort(sortBy('lastName'));
+		listaTrabajadores = listaTrabajadores.map((trabajador, i) => { return <Trabajador key={i} data={trabajador} />});
+
+		return(
+			<ul>
+				{listaTrabajadores}
+			</ul>
+		);
+	}
+});
+module.exports = ListaTrabajadores;
